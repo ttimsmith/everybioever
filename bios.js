15fb77e497b645e6ff594e32aca3b536f44aca97
Galen Gidman <galen@galengidman.com>
2013-04-08 13:17:43 -0500

    wups, forgot to delete bios.js :(

--- bios.js
+++
@@ -1,8 +0,0 @@
-var bios = new Array(
-	'Nerd.',
-	'Husband, father, designer and writer. Craft beer enthusiast.',
-	'I FOllow Back!!!!',
-	'Justin <3<3<3',
-	'Opinionated',
-	'Retweets are not endorsements.'
-);
